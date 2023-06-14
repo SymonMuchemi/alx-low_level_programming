df0bf1c3d8ba73f894cb4115f21b28febd042e8f
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-15 00:01:48 +0300

    task 0

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - function declaration
+ * @dest: destination to copy
+ * @src: source to copy
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strcpy - function declaration
+ * Description: copies a string
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	(void)src;
+	
+	return (dest);
+}
