6d158920425c15671a123bb799df8c433988b10e
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-15 21:48:59 +0300

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked(unsigned int b) - allocates memory using malloc()
+ * @b: memory size
+ * Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+    void *mem_location = malloc(b);
+
+    if (mem_location == NULL)
+        exit(98);
+    
+    return mem_location;
+}
