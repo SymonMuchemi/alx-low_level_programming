77a9384869ef997bdb8e8de1a2aa551208d19ed8
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-20 00:17:22 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+
+#endif
