dacc6e790ddbb34f0a442e18bb4831051646605a
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-19 21:16:05 +0300

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * ABS(x) - computes the absolute value of a variable
+ * @x: the variable
+*/
+#define ABS(x) ((x) < (0)? (-x) : (x))
+
+#endif
