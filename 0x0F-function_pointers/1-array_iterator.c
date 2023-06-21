72dbb8bbab42ff79aef53aed84d3d3fd190a34d3
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-21 18:05:38 +0300

    task 1 ---handling-null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
     size_t i = 0;
 
+    if(array == NULL || action == NULL)
+        return;
+
     while (i < size)
     {
         action(array[i]);
