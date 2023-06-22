ec31cc6b537ef2242ff565a0bf582ff3a8824ce2
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-23 00:03:31 +0300

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,98 @@
 #include "variadic_functions.h"
 /**
- * print_all - prints anything with a specified format
- * @format: format to print in
- * Return: nothing
-*/
+ * struct op - struct for 3
+ * @c: character to check
+ * @f: function to point to
+ */
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list a);
+} printer;
+/**
+ * print_c - prints char
+ * @a: list to give
+ * Return: always 0
+ */
+int print_c(va_list a)
+{
+	printf("%c", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_i - prints int
+ * @a: list to give
+ * Return: always 0
+ */
+int print_i(va_list a)
+{
+	printf("%d", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_f - prints float
+ * @a: list to give
+ * Return: always 0
+ */
+int print_f(va_list a)
+{
+	printf("%f", va_arg(a, double));
+	return (0);
+}
+/**
+ * print_s - prints string
+ * @a: list to give
+ * Return: always 0
+ */
+int print_s(va_list a)
+{
+	char *s;
+
+	s = va_arg(a, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return (0);
+	}
+	printf("%s", s);
+	return (0);
+}
+/**
+ * print_all - prints all
+ * @format: format string that says arg types
+ *
+ */
 void print_all(const char * const format, ...)
 {
-    unsigned int i = 0;
-    char *str;
-    char c;
-    int num;
-    float fnum;
-    char *nil = "(nil)";
+	int i, j;
+	char *sep = "";
+	char *sep2 = ", ";
+	va_list anyArgs;
+	printer ops[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"s", print_s},
+		{"f", print_f},
+		{NULL, NULL}
+	};
 
-    va_list args;
-    va_start(args, format);
-    /*loop over each character in the format string*/
-    while(format && format[i])
-    {
-         if (format[i] == 'c')
-        {
-            c = va_arg(args, int); /*Check*/
-            printf("%c", c);
-        }
-        else if (format[i] == 'i')
-        {
-            num = va_arg(args, int);
-            printf("%d", num);
-        }
-        else if (format[i] == 'f')
-        {
-            fnum = va_arg(args, double);
-            printf("%f", fnum);
-        }
-        else if (format[i] == 's')
-        {
-            str = va_arg(args, char *);
-            if (str == NULL)
-                printf("%s", nil);
-            else
-                printf("%s", str);
-        }
-        i++;
-    }
-    va_end(args);
-    printf("\n");
+	va_start(anyArgs, format);
+	i = 0;
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (ops[j].f != NULL)
+		{
+			if (format[i] == *(ops[j].c))
+			{
+				printf("%s", sep);
+				ops[j].f(anyArgs);
+			}
+			j++;
+		}
+		sep = sep2;
+		i++;
+	}
+	printf("\n");
+	va_end(anyArgs);
 }
