b4a760c7b74fd249e058aa7b493f85e3853932ab
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-22 14:10:37 +0300

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,23 +8,23 @@ int main(int argc, char const *argv[])
 {
     int num1, num2, result;
     int (*operation)(int, int);
-    char operand[] = argv[1];
 
-    if ( argc != 3)
-        return (-1);
-    
-    if (!checkForDigitsOnly(argv[0]))
-        return (-1);
-    
-    if (!checkForDigitsOnly(argv[2]))
-        return (-1);
-    
-    num1 = atoi(argv[0]);
-    num2 = atoi(argv[2]);
+    while (argc == 4)
+    {
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+        
+        operation = get_op_func(argv[2]);
 
-    operation = get_op_func(operand);
-
-    result = operation(num1, num2);
-
-    return 0;
+        result = operation(num1, num2);
+        if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+        printf("Error\n");
+        return (0);
+    }
+    printf("Error\n");
+    exit(98);    
 }
