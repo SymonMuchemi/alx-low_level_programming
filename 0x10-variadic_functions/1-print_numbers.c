e64bfab642bc6b41843464b2c7d585e8a7fc6486
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-22 16:16:03 +0300

    Task 1.2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     va_list arg_list;
     va_start(arg_list, n);
 
-    if (n == 0 || separator == NULL)
+    if (n <= 0 || separator == NULL)
         return;
 
     for (i = 0; i < n; i++)
