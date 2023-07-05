948cdf3c848c6b9e4fd5f12f55f899e8e1540b45
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-05 03:07:19 +0300

    task 7 ---while-head

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
     if (head == NULL)
         return (NULL);
     
-    while (1)
+    while (head)
     {
         if (count == index)
             return (head);
