2099175f5a48688f6ea8e20d9dc5d4675d22d174
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-05 04:36:10 +0300

    task 9 --minor fixing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,38 +1,47 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - inserts a node at a given index
- * @head: pointer to the head node
- * @idx: index
- * @n: data to hold
- * Return: address of new node or NULL
+ * insert_nodeint_at_index - function with 3 arguments
+ * @head: pointer to head pointer of first node in linked list
+ * @idx: index of list
+ * @n: value integer
+ *
+ * Description: inserts a new node at a given position
+ * Return: address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *newNode, *nthNode, *currentNode;
+	listint_t *newNode, *currentNode;
+	unsigned int count = 0;
 
-    newNode = (listint_t *)malloc(sizeof(listint_t));
-    if (newNode == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    newNode->n = n;
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	currentNode = *head;
 
-    if (idx == 0)
-    {
-        newNode->next = *head;
-        *head = newNode;
-        return (newNode);
-    }
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (*head);
+	}
 
-    nthNode = get_nodeint_at_index(*head, idx - 1);
-    if (nthNode == NULL)
-    {
-        free(newNode);
-        return (NULL);
-    }
+	while (currentNode != NULL)
+	{
+		if (count == idx - 1)
+		{
+			newNode->next = currentNode->next;
+			currentNode->next = newNode;
+		}
+		count++;
+		currentNode = currentNode->next;
+	}
+	if (idx > count)
+		return (NULL);
+	return (newNode);
 
-    currentNode = nthNode->next;
-    nthNode->next = newNode;
-    newNode->next = currentNode;
-
-    return (newNode);
 }
