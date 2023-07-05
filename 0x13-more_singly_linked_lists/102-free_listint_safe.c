b06bb4c218aac882ad3e1031414a3a383a5b55ea
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-05 04:56:45 +0300

    task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,13 +9,13 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *runner, *head;
+	listint_t *currentNode, *runner, *head;
 	size_t h_i, r_i;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	current = *h;
+	currentNode = *h;
 	head = *h;
 	h_i = 0;
 
@@ -24,16 +24,16 @@ size_t free_listint_safe(listint_t **h)
 		runner = *h;
 		for (r_i = 0; r_i < h_i; r_i++)
 		{
-			if (runner == current)
+			if (runner == currentNode)
 			{
 				*h = NULL;
 				return (h_i);
 			}
 			runner = runner->next;
 		}
-		current = head->next;
+		currentNode = head->next;
 		free(head);
-		head = current;
+		head = currentNode;
 		h_i++;
 	}
 	*h = NULL;
