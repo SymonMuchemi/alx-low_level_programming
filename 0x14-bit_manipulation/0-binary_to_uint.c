d50c7a31728d7ab05717dd85d100b534d0555b95
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-07 17:06:35 +0300

    task 0 --error-handling

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
 /**
  * binary_to_uint - converts a binary number to 
  * unsigned int
  * @b: binary rep of number
  * Return: decimal number or 0
  */
-
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int num = 0;
     unsigned int i = 0;
+
+    if (b == NULL)
+        return (0);
 
     while (b[i] != '\0')
     {
