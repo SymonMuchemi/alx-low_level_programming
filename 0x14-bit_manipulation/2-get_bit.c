d441440cf8d8ab8cd102b3dd42d51a6c8464e2bd
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-07 17:52:40 +0300

    task 2 ---error-handling

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
     int x = n;
 
-    if (index >= sizeof(unsigned long int) * 8);
+    if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
     
     x = (x & (1 << index)) >> index;
 
