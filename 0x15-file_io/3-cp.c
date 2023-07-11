18bc14cf20eba8c137b87ca2272914cd4735d2ea
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-12 02:14:06 +0300

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ * _cp - copies file contents of one file to another
+ * @file_from: file to copy content from
+ * @file_to: file to paste content to
+ * Return: void
+ */
+void _cp(char *file_from, char *file_to)
+{
+    int from_fd, to_fd;
+    char *buff[1024];
+    ssize_t bytesWritten, bytesRead;
+
+    if (!file_from)
+        exit(98);
+    
+    from_fd = open(file_from, O_RDONLY);
+    bytesRead = read(from_fd, buff, 1024);
+
+    if (bytesRead == -1 || from_fd == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't read from file %s", file_from);
+        exit(98);
+    }
+
+    to_fd = open(file_to, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    bytesWritten = write(to_fd, buff, bytesRead);
+    if (bytesWritten == -1 || to_fd == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't write to %s", file_from);
+        exit(99);
+    }
+    if(close(from_fd) == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't close fd %d", from_fd);
+        exit(100);
+    }
+
+    if(close(to_fd) == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't close fd %d", to_fd);
+        exit(100);
+    }
+}
+/**
+ * main -  program that copies the content of 
+ * a file to another file.
+ * @argc: argument count 
+ * @argv: argument vector 
+ * Return: int 
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        dprintf(STDOUT_FILENO, "Usage: cp file_from file_to");
+        exit(97);
+    }
+    
+    _cp(argv[1], argv[2]);
+
+    return 0;
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,10 +8,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFSIZE 1024
+#define _GNU_SOURCE
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+void _cp(char *file_from, char *file_to);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
