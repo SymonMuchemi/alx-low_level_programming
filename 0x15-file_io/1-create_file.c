96cc3c1d2b0edd5ad8c005f388e9dc2121d47953
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-11 04:06:00 +0300

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: pointer to file
+ * @text_content: NULL terminated string to 
+ * write to the file
+ * Return: 1 on success or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd, len = 0;
+    ssize_t written;
+
+    if (filename == NULL)
+        return (-1);
+
+    /*Get length of string*/
+    while (text_content[len])
+    {
+        len++;
+    }
+
+    fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+    written = write(fd, text_content, len);
+
+    if (fd == -1 || written == -1)
+        return (-1);
+    
+    return (1);
+}
