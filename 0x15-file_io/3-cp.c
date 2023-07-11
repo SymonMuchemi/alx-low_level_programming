5bde4c1f899537a77232060e00c1d30f40acd022
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-12 02:17:52 +0300

    header files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,9 @@
-#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
  * _cp - copies file contents of one file to another
  * @file_from: file to copy content from
