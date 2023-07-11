62fbdce823fdb6875e60ebaf161ea5001c4b0a65
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-11 04:08:44 +0300

    check content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
     int fd, len = 0;
     ssize_t written;
 
-    if (filename == NULL)
+    if (filename == NULL || text_content == NULL)
         return (-1);
 
     /*Get length of string*/
