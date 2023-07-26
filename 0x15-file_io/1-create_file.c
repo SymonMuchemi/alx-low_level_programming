7c4cd4daa4450b9f6b5c011623bfdb6010a0b517
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-27 00:06:54 +0300

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len = 0;
-    ssize_t written;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (filename == NULL || text_content == NULL)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    /*Get length of string*/
-    while (text_content[len])
-    {
-        len++;
-    }
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-    written = write(fd, text_content, len);
+	if (fd == -1)
+		return (-1);
 
-    if (fd == -1 || written == -1)
-        return (-1);
-    
-    close(fd);
-    return (1);
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
