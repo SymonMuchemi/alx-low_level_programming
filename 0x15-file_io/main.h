f397b08c86850c9256c6cc9048321eb24b9d023b
SymonMuchemi <simonkenya846@gmail.com>
2023-10-31 02:27:32 +0300

    header fille update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,11 @@
 #include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+char *create_buf(char *filename);
+void close_file(int fd);
+void error_file(int file_from, int file_to, char *argv[]);
 
 #endif
