3a98e3d906f1faaddd81f4758e7ee338e1f12bf0
SymonMuchemi <simonkenya846@gmail.com>
2023-12-11 15:15:14 +0300

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes in the list
+ * @head: pointer to the head node
+ * Return: nothing
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head->next)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
