e15077004229621a94d46a2a445d8f9fbe17906c
SymonMuchemi <simonkenya846@gmail.com>
2023-12-14 13:19:59 +0300

    !head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *newNode, *previous, *temp;
 
-	if (!(*head))
+	if (!(*head) || !head)
 		return (NULL);
 
 	temp = *head;
