5054f3ac70a25f34a963f518e5c95c199b6d8c00
SymonMuchemi <simonkenya846@gmail.com>
2024-01-18 01:08:56 +0300

    *

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-	hash_node_t *array = malloc(sizeof(hash_node_t) * size);
+	hash_node_t *array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table == NULL || array == NULL)
 	{
