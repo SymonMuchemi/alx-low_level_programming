d292a3ccaf7c468e0a7bca7d07b6014daf110a24
SymonMuchemi <simonkenya846@gmail.com>
2024-01-22 03:01:46 +0300

    addition operation with error handling and same key handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,19 @@
 #include "hash_tables.h"
 
 /**
+ * free_node - Free a node.
+ * @node: Node to free.
+ *
+ * Return: Void.
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table
  * @key: the key string, unique to each element
@@ -9,32 +22,57 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned int index;
+	hash_node_t *new_node, *current;
 
-	/* creating a new hash node to store key-value*/
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
-	if (!new_node || ht == NULL)
+	if (strcmp(key, "") == 0 || ht == NULL || strcmp(value, "") == 0)
 	{
 		return (0);
 	}
 
-	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 
+	/* populate data to the new node*/
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
-	/* chack for collision*/
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+		return (1);
 	}
 
+	/* incase of a collision */
+	current = ht->array[index];
+
+	/* if the keys are the same, replace nodes*/
+	if (strcmp(ht->array[index]->key, key) == 0)
+	{
+		new_node->next = current->next;
+		ht->array[index] = new_node;
+		free_node(current);
+		return (1);
+	}
+
+	/* search for node with same key in the index*/
+	while (current->next != NULL && strcmp(current->next->key, key) != 0)
+	{
+		current = current->next;
+	}
+
+	/* if node with same key is found, replace*/
+	if (strcmp(current->key, key) == 0)
+	{
+		new_node->next = current->next->next;
+		free_node(current->next);
+		current->next = new_node;
+		return (1);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
