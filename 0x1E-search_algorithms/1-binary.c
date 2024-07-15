d0a79f9054c68624bbe0a7c12787e83125920e56
symonmuchemi <simonkenya846@gmail.com>
2024-07-15 17:06:57 +0300

    Task 1
    implemented the binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for an element using the binary search alg
+ * @array: the sequence of integer elements
+ * @size: the number of elements to look at in the array
+ * @value: the element being searched
+ * Return: the first index the element appears or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = ((int) size) - 1;
+	int mid;
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		print_subarray(array, left, right);
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return (-1);
+}
+/**
+ * print_subarray - prints out a part of an array
+ * @arr: the parent array
+ * @left: the first index
+ * @right: the last index
+ * Return: nothing, just printing
+ */
+void print_subarray(int arr[], int left, int right)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", arr[i]);
+	printf("%d\n", i);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,6 +20,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_subarray(int arr[], int left, int right);
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
