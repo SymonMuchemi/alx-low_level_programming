8bc4ce7ad27e9a34bb52fc66641389e5556283c9
symonmuchemi <simonkenya846@gmail.com>
2024-07-15 18:14:51 +0300

    very funny bug

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,14 +10,14 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int len = (int) size;
-	int jump_size = sqrt(len);
+	int len = (int) size - 1;
+	int jump_size = (int) sqrt(size);
 	int i, k;
 
 	if (!array)
 		return (-1);
 
-	for (i = 0; i < len - 1; i += jump_size)
+	for (i = 0; i <= len; i += jump_size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
@@ -27,9 +27,11 @@ int jump_search(int *array, size_t size, int value)
 			break;
 	}
 
-	for (k = i - jump_size; k < len - 1; i += 1)
+	printf("Value found between indexes [%d] and [%d]\n", i, i + jump_size);
+
+	for (k = i - jump_size; k <= len; k++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", k, array[k]);
 		if (array[k] == value)
 			return (k);
 	}
