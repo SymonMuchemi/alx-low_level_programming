3b4c65dc7f5bf80e6c3efd06e48c75cb271fcdcc
symonmuchemi <simonkenya846@gmail.com>
2024-07-15 16:24:43 +0300

    task 0
    Implemented the linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array using linear search alg
+ * @array: the sequence of numbers
+ * @size: the number of elements to search from
+ * @value: the value to look for
+ * Return: the first index of the value element or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("value checked array [%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
