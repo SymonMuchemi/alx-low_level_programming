e15985521526d57d2f14e6a9777311c52172808e
symonmuchemi <simonkenya846@gmail.com>
2024-07-18 04:14:58 +0300

    update: caps and size check

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,12 +11,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("value checked array [%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
