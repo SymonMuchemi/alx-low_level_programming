e2e5e6d6ec3369b35bd631056cf46c65861376f8
symonmuchemi <simonkenya846@gmail.com>
2024-07-20 23:22:02 +0300

    maybe changing the return type will work

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (i);
+			return ((int) i);
 	}
 
 	return (-1);
